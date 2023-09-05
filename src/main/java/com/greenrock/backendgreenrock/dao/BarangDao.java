8ef870e6fd6a34021ddf2e940be60065a90d6dd4
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-09-06 00:17:48 +0800

    feat: add barang dao

---
+++ src/main/java/com/greenrock/backendgreenrock/dao/BarangDao.java
@@ -0,0 +1,9 @@
+package com.greenrock.backendgreenrock.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greenrock.backendgreenrock.entity.Barang;
+
+public interface BarangDao extends JpaRepository<Barang, Integer> {
+
+}
