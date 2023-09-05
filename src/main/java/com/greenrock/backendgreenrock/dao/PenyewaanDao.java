f6fc72866440c55253bfb753f5e8802ffa8e53a5
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-09-06 00:18:00 +0800

    feat: add penyewaan

---
+++ src/main/java/com/greenrock/backendgreenrock/dao/PenyewaanDao.java
@@ -0,0 +1,9 @@
+package com.greenrock.backendgreenrock.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greenrock.backendgreenrock.entity.Penyewaan;
+
+public interface PenyewaanDao extends JpaRepository<Penyewaan, String> {
+
+}
